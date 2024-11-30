996917860e4620d1bd8e87230f5fc24629f56aad
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2024-11-30 14:35:10 +0000

    Fix warnings + Add unittests + Considerable improvements

--- AAEmu.Game/Core/Managers/Id/WorldIdManager.cs
+++ AAEmu.Game/Core/Managers/Id/WorldIdManager.cs
@@ -7,7 +7,7 @@ public class WorldIdManager : IdManager
     private static WorldIdManager _instance;
     private const uint FirstId = 0x00000064;
     private const uint LastId = 0xFFFFFFFE;
-    private static readonly uint[] Exclude = { };
+    private static readonly uint[] Exclude = [];
     private static readonly string[,] ObjTables = { { } };
 
     public static WorldIdManager Instance => _instance ?? (_instance = new WorldIdManager());
